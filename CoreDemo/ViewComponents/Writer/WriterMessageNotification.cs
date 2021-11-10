135d65e8ff9394ff5b85b9829a48b9c8b4708d22
burak551 <70866473+burak551@users.noreply.github.com>
2021-11-10 19:42:31 +0300

    Yazar mesaj bildirim surelerindeki hata duzeltildi.

--- CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
+++ CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -15,7 +15,7 @@ namespace CoreDemo.ViewComponents.Writer
         {
             var values = notificationManager.
                 GetList(x => x.ReceiverMail == "temelkayaburak@gmail.com" &&
-                x.MessageStatus == true && x.MessageDate.Day == DateTime.Now.Day);
+                x.MessageStatus == true);
             if (values.Count() > 3)
             {
                 values = values.Take(3).ToList();
