d8db98377f7b28f65e48fe8634f7d0574e274a36
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2022-09-13 00:31:05 +0300

    150.Ders

---
+++ Jwt_Core_Proje_Kampi/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using Jwt_Core_Proje_Kampi.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jwt_Core_Proje_Kampi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : Controller
+    {
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Page1()
+        {
+            return Ok("Sayfa 1 için girişiniz başarılı");
+        }
+    }
+}
