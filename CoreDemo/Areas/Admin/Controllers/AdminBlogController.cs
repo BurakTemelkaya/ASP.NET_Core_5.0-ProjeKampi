2d856f3df09cbd80b80647e240e740c7721673b7
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2023-02-24 06:15:36 +0300

    Hata düzeltmeleri.

--- CoreDemo/Areas/Admin/Controllers/AdminBlogController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminBlogController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using X.PagedList;
