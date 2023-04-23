c1746dc227e8452d5bbd63629c7ccd7c9f464941
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2023-04-24 02:41:20 +0300

    karanlık tema dosyaları eklendi.

---
+++ CoreDemo/Controllers/ThemaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CoreDemo.Controllers
+{
+    public class ThemaController : Controller
+    {
+        [HttpPost]
+        public IActionResult SetThema(string data)
+        {
+            CookieOptions cookies = new();
+            cookies.Expires=DateTime.Now.AddYears(1);
+            Response.Cookies.Append("thema", data, cookies);
+            return View();
+        }
+    }
+}
