cff332cd541ff3c44bc320c8a607be3b3131002b
Burak TEMELKAYA <burak_istanbul_07@hotmail.com>
2023-07-12 00:31:32 +0300

    admin panelinde okunmamış mesaj sayısının 2 kez backend'den gelmesi düzeltildi.

--- CoreDemo/wwwroot/js/admin-folders.js
+++ CoreDemo/wwwroot/js/admin-folders.js
@@ -1,18 +1,29 @@
-﻿
-function GetMessageListForDropDown() {
+﻿let isUnreadMessage;
+
+function GetUnreadMessageCount() {
     $(document).ready(function () {
         $.ajax({
             url: '/Admin/AdminMessage/GetUnreadMessagesCount',
             type: "GET",
             success: function (data) {
-                let badgeHtml = '<i class="fa fa-envelope">';
                 if (data != '0') {
-                    badgeHtml += '<span class="label label-warning">' + data + '</span>';
+                    isUnreadMessage = true;
                 }
-                badgeHtml += '</i>';
-                $("#UnreadMessagesCountBadge").html(badgeHtml);
             }
         });
+    });
+}
+
+
+GetUnreadMessageCount();
+function GetMessageListForDropDown() {
+    $(document).ready(function () {
+        let badgeHtml = '<i class="fa fa-envelope">';
+        if (isUnreadMessage) {
+            badgeHtml += '<span class="label label-warning">' + data + '</span>';
+        }
+        badgeHtml += '</i>';
+        $("#UnreadMessagesCountBadge").html(badgeHtml);
     });
 };
 
@@ -20,20 +31,12 @@ GetMessageListForDropDown();
 
 function GetMessageListForMessageFolder() {
     $(document).ready(function () {
-        $.ajax({
-            url: '/Admin/AdminMessage/GetUnreadMessagesCount',
-            type: "GET",
-            success: function (data) {
-                let badgeHtml = '<i class="fa fa-inbox "></i> Gelen Mesajlar ';
-                if (data != '0') {
-                    badgeHtml += '<span class="label label-warning float-right">' + data + '</span>';
-                }
-                badgeHtml += '</i>';
-                $("#messageFolderBadge").html(badgeHtml);
-            }
-        });
-
-
+        let badgeHtml = '<i class="fa fa-inbox "></i> Gelen Mesajlar ';
+        if (isUnreadMessage != null) {
+            badgeHtml += '<span class="label label-warning float-right">' + data + '</span>';
+        }
+        badgeHtml += '</i>';
+        $("#messageFolderBadge").html(badgeHtml);
     });
 };
 
