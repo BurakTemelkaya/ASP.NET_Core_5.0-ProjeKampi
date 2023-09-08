f2375bc6e5d40dcd60f5a1f1dd70f0a9b86e5472
Burak TEMELKAYA <burak_istanbul_07@hotmail.com>
2023-09-08 20:27:17 +0300

    merge

---
+++ CoreDemo/Worker.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreDemo;
+
+public class Worker : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                string url = "https://buraktemelkaya.com";
+
+                using (HttpClient client = new())
+                {
+                    using (HttpResponseMessage response = await client.GetAsync(url, cancellationToken))
+                    {
+                        using (HttpContent content = response.Content)
+                        {
+                            string result = await content.ReadAsStringAsync(cancellationToken);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            await Task.Delay(TimeSpan.FromMinutes(5), cancellationToken);
+        }
+    }
+}
