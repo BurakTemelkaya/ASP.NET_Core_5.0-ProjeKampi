e15cc2ed1ae8ddc5b7e03d34553808ad0344f267
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2025-06-06 00:24:08 +0300

    Newsletter hata düzeltmesi.

--- BusinessLayer/Concrete/NewsLetterManager.cs
+++ BusinessLayer/Concrete/NewsLetterManager.cs
@@ -7,99 +7,96 @@ using CoreLayer.Utilities.MailUtilities;
 using CoreLayer.Utilities.Results;
 using DataAccessLayer.Abstract;
 using EntityLayer.Concrete;
-using EntityLayer.DTO;
 using MimeKit;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
-namespace BusinessLayer.Concrete
+namespace BusinessLayer.Concrete;
+
+public class NewsLetterManager : INewsLetterService
 {
-    public class NewsLetterManager : INewsLetterService
+    private readonly INewsLetterDal _newsLetterDal;
+    readonly IMailService _mailService;
+
+    public NewsLetterManager(INewsLetterDal newsLetterDal, IMailService mailService)
     {
-        private readonly INewsLetterDal _newsLetterDal;
-        readonly IMailService _mailService;
+        _newsLetterDal = newsLetterDal;
+        _mailService = mailService;
+    }
 
-        public NewsLetterManager(INewsLetterDal newsLetterDal, IMailService mailService)
+    public async Task<IDataResult<NewsLetter>> GetByMailAsync(string mail)
+    {
+        return new SuccessDataResult<NewsLetter>(await _newsLetterDal.GetByFilterAsync(x => x.Mail == mail));
+    }
+
+    public async Task<IDataResult<int>> GetCountAsync()
+    {
+        return new SuccessDataResult<int>(await _newsLetterDal.GetCountAsync());
+    }
+
+    public async Task<IDataResult<List<NewsLetter>>> GetListAsync()
+    {
+        return new SuccessDataResult<List<NewsLetter>>(await _newsLetterDal.GetListAllAsync());
+    }
+
+    public async Task<IResultObject> SendMailAsync(NewsLetterSendMailsModel model, bool mailStatus)
+    {
+        List<NewsLetter> newsLetters = await _newsLetterDal.GetListAllAsync(x => x.MailStatus);
+
+        foreach (var newsLetter in newsLetters)
         {
-            _newsLetterDal = newsLetterDal;
-            _mailService = mailService;
-        }
-
-        public async Task<IDataResult<NewsLetter>> GetByMailAsync(string mail)
-        {
-            return new SuccessDataResult<NewsLetter>(await _newsLetterDal.GetByFilterAsync(x => x.Mail == mail));
-        }
-
-        public async Task<IDataResult<int>> GetCountAsync()
-        {
-            return new SuccessDataResult<int>(await _newsLetterDal.GetCountAsync());
-        }
-
-        public async Task<IDataResult<List<NewsLetter>>> GetListAsync()
-        {
-            return new SuccessDataResult<List<NewsLetter>>(await _newsLetterDal.GetListAllAsync());
-        }
-
-        public async Task<IResultObject> SendMailAsync(NewsLetterSendMailsModel model, bool mailStatus)
-        {
-            var newsLetters = await _newsLetterDal.GetListAllAsync(x => x.MailStatus == mailStatus);
-
-            List<MailboxAddress> mailboxAddresses = new();
-
-            foreach (var newsLetter in newsLetters)
-            {
-                mailboxAddresses.Add(new MailboxAddress(newsLetter.Mail, newsLetter.Mail));
-            }
-
             await _mailService.SendEmailAsync(new Mail()
             {
-                ToList = mailboxAddresses,
-                HtmlBody = MailTemplates.BanOpenUserContentTemplate(),
+                ToList =
+                [
+                    new MailboxAddress(newsLetter.Mail, newsLetter.Mail)
+                ],
+                HtmlBody = model.Content,
                 Subject = model.Subject,
             });
+        }
+        
 
+        return new SuccessResult();
+    }
+
+    [ValidationAspect(typeof(NewsLetterValidator))]
+    public async Task<IResultObject> TAddAsync(NewsLetter t)
+    {
+        var mail = await GetByMailAsync(t.Mail);
+
+        if (mail.Data == null)
+        {
+            t.MailStatus = true;
+            await _newsLetterDal.InsertAsync(t);
             return new SuccessResult();
         }
 
-        [ValidationAspect(typeof(NewsLetterValidator))]
-        public async Task<IResultObject> TAddAsync(NewsLetter t)
-        {
-            var mail = await GetByMailAsync(t.Mail);
+        return new ErrorResult(Messages.NewsLetterAlreadyRegistered);
+    }
 
-            if (mail.Data == null)
-            {
-                t.MailStatus = true;
-                await _newsLetterDal.InsertAsync(t);
-                return new SuccessResult();
-            }
+    public async Task<IResultObject> TDeleteAsync(NewsLetter t)
+    {
+        await _newsLetterDal.DeleteAsync(t);
+        return new SuccessResult();
+    }
 
-            return new ErrorResult(Messages.NewsLetterAlreadyRegistered);
-        }
+    public async Task<IDataResult<NewsLetter>> TGetByFilterAsync(Expression<Func<NewsLetter, bool>> filter = null)
+    {
+        return new SuccessDataResult<NewsLetter>(await _newsLetterDal.GetByFilterAsync(filter));
+    }
 
-        public async Task<IResultObject> TDeleteAsync(NewsLetter t)
-        {
-            await _newsLetterDal.DeleteAsync(t);
-            return new SuccessResult();
-        }
+    public async Task<IDataResult<NewsLetter>> TGetByIDAsync(int id)
+    {
+        return new SuccessDataResult<NewsLetter>(await _newsLetterDal.GetByIDAsync(id));
+    }
 
-        public async Task<IDataResult<NewsLetter>> TGetByFilterAsync(Expression<Func<NewsLetter, bool>> filter = null)
-        {
-            return new SuccessDataResult<NewsLetter>(await _newsLetterDal.GetByFilterAsync(filter));
-        }
-
-        public async Task<IDataResult<NewsLetter>> TGetByIDAsync(int id)
-        {
-            return new SuccessDataResult<NewsLetter>(await _newsLetterDal.GetByIDAsync(id));
-        }
-
-        [ValidationAspect(typeof(NewsLetterValidator))]
-        public async Task<IResultObject> TUpdateAsync(NewsLetter t)
-        {
-            await _newsLetterDal.UpdateAsync(t);
-            return new SuccessResult();
-        }
+    [ValidationAspect(typeof(NewsLetterValidator))]
+    public async Task<IResultObject> TUpdateAsync(NewsLetter t)
+    {
+        await _newsLetterDal.UpdateAsync(t);
+        return new SuccessResult();
     }
 }
--- CoreLayer/Utilities/CaptchaUtilities/ICaptchaService.cs
+++ CoreLayer/Utilities/CaptchaUtilities/ICaptchaService.cs
@@ -1,12 +1,11 @@
 ﻿using System.Threading.Tasks;
 
-namespace CoreLayer.Utilities.CaptchaUtilities
+namespace CoreLayer.Utilities.CaptchaUtilities;
+
+public interface ICaptchaService
 {
-    public interface ICaptchaService
-    {
-        Task<bool> CheckCaptchaValidate(string captcharesponse = null);
-        Task<string> RecaptchaControl(string captcharesponse = null);
-        public string GetSiteKey();
-        public string GetSecretKey();
-    }
+    Task<bool> CheckCaptchaValidate(string captcharesponse = null);
+    Task<string> RecaptchaControl(string captcharesponse = null);
+    public string GetSiteKey();
+    public string GetSecretKey();
 }
